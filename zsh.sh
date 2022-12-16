7b476b8e74c85319484aad0971bbcba166e8cfe6
Akuma <86250671+rep-aku@users.noreply.github.com>
2022-12-16 19:47:40 +0100

    Custom-zsh

---
+++ zsh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/ && git clone https://github.com/zsh-users/zsh-completions.git \
+  ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+ cd ~/Desktop &&  curl -L https://raw.githubusercontent.com/rep-aku/Custom-zsh/main/.zshrc  > .zshrc
