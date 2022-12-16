d7b3a730a0a7e615934c8bbd73467835fe6c7734
Akuma <86250671+rep-aku@users.noreply.github.com>
2022-12-16 19:50:02 +0100

    Custom-zsh

--- zsh.sh
+++ zsh.sh
@@ -7,3 +7,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 git clone https://github.com/zsh-users/zsh-autosuggestions \
   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
  cd ~/ &&  curl -L https://raw.githubusercontent.com/rep-aku/Custom-zsh/main/.zshrc  > .zshrc
+ curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
+ brew install lsd
+ brew install bat
